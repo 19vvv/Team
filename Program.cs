5bfe3fb44ee9f4c67213d1b5d781c611750a93d7
adahm0 <danah056524@gmail.com>
2024-11-06 14:24:03 +0300

    name

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("HellovFutoon");
 Console.WriteLine("Hello Norah ");
+Console.WriteLine("Hello aldanah ");
+
--- obj/Debug/net8.0/Team.AssemblyInfo.cs
+++ obj/Debug/net8.0/Team.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Team")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef55310d61ac8c68bab058e48048500e0caef16a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56eaf64827c39e7f892e1224de124c8a7a19f7b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Team")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Team")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
