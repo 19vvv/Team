3376adfff90a10b30c9cb0ffef9ba64b24fcde89
19vvv <futoon099@gmail.com>
2024-12-26 14:57:27 +0300

    hh

--- obj/Debug/net8.0/Team.AssemblyInfo.cs
+++ obj/Debug/net8.0/Team.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Team")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7d2d4b42e2b5be9f47b95b412f845e4b3722401")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4e984bce2eb91a24ce93cc9b89a6609d1648634")]
 [assembly: System.Reflection.AssemblyProductAttribute("Team")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Team")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
